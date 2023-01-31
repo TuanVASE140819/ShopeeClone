fa07fc7e65d58e94695603026322d2d3cf5c516a
Vu Anh Tuan <tuanvuanhse140819@gmail.com>
2023-01-31 15:05:38 +0700

    change class. con by pubfg

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,11 @@
+const theme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  corePlugins: {
+    container: false
+  },
   theme: {
     extend: {
       colors: {
@@ -8,5 +13,16 @@ module.exports = {
       }
     }
   },
-  plugins: []
+  plugins: [
+    plugins(function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: theme('columns.7xl'),
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          paddingLeft: theme('spacing.4')
+        }
+      })
+    })
+  ]
 }
