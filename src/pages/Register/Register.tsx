1b4532a640aa58814af6646f255d1096512299c2
Vu Anh Tuan <tuanvuanhse140819@gmail.com>
2023-01-31 22:45:08 +0700

    create component Input

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,5 +1,6 @@
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
+import Input from 'src/components/Input'
 import { getRules } from 'src/utils/rules'
 
 interface FormData {
@@ -37,37 +38,35 @@ export default function Register() {
           <div className='lg:col-span-2 lg:col-start-4'>
             <form className='rounded bg-white p-10 shadow-sm' onSubmit={onSubmit} noValidate>
               <div className='text-2xl'>Đăng ký</div>
-              <div className='mt-8'>
-                <input
-                  type='email'
-                  className='focus::border-gray-500 rouded-sm w-full border border-gray-300 p-3 outline-none focus:shadow-sm'
-                  placeholder='Email'
-                  {...register('email', rules.email)}
-                />
-                <div className='.min-h-[1.25rem].text-sm mt-1 text-red-600'>{errors.email?.message}</div>
-              </div>
-              <div className='mt-3'>
-                <input
-                  type='password'
-                  className='focus::border-gray-500 rouded-sm w-full border border-gray-300 p-3 outline-none focus:shadow-sm'
-                  placeholder='password'
-                  autoComplete='on'
-                  {...register('password', rules.password)}
-                />
-                <div className='.min-h-[1.25rem].text-sm mt-1 text-red-600'>{errors.password?.message}</div>
-              </div>
-              <div className='mt-3'>
-                <input
-                  type='password'
-                  className='focus::border-gray-500 rouded-sm w-full border border-gray-300 p-3 outline-none focus:shadow-sm'
-                  placeholder='Comfirm Password'
-                  autoComplete='on'
-                  {...register('comfirm_password', {
-                    ...rules.comfirm_password
-                  })}
-                />
-                <div className='.min-h-[1.25rem].text-sm mt-1 text-red-600'>{errors.comfirm_password?.message}</div>
-              </div>
+              <Input
+                name='email'
+                register={register}
+                type='email'
+                className='mt-8'
+                errorMessage={errors.email?.message}
+                placeholder='Email'
+                rules={rules.email}
+              />
+              <Input
+                name='password'
+                register={register}
+                type='password'
+                className='mt-3'
+                autoComplete='on'
+                errorMessage={errors.password?.message}
+                placeholder='password'
+                rules={rules.password}
+              />
+              <Input
+                name='comfirm_password'
+                register={register}
+                type='password'
+                autoComplete='on'
+                className='mt-3'
+                errorMessage={errors.comfirm_password?.message}
+                placeholder='Comfirm Password'
+                rules={rules.comfirm_password}
+              />
               <div className='mt-3'>
                 <button className='lex  w-full items-center justify-center bg-red-500 py-4 px-2 text-sm uppercase text-white hover:bg-red-600'>
                   Đăng ký
