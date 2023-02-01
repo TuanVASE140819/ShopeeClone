f4e0f6f4d84cbf0cd8d5c3990e452adc15873676
Vu Anh Tuan <tuanvuanhse140819@gmail.com>
2023-02-01 23:41:51 +0700

    toast đơn thuần

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,4 +1,6 @@
-import axios, { type AxiosInstance } from 'axios'
+import axios, { AxiosError, type AxiosInstance } from 'axios'
+import HttpStatusCode from '../constants/httpStatusCode.enum'
+import { toast } from 'react-toastify'
 
 class Http {
   instance: AxiosInstance
@@ -10,6 +12,23 @@ class Http {
         'Content-Type': 'application/json'
       }
     })
+    this.instance.interceptors.request.use(
+      function (response) {
+        return response
+      },
+      function (error: AxiosError) {
+        if (
+          ![HttpStatusCode.UnprocessableEntity, HttpStatusCode.Unauthorized].includes(error.response?.status as number)
+        ) {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          const data: any | undefined = error.response?.data
+          const message = data?.message || error.message
+          toast.error(message)
+        }
+
+        return Promise.reject(error)
+      }
+    )
   }
 }
 
